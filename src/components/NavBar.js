2076eb74355e91fec0cb709bfd73279845ec700d
Barrionuebodamian <barrionuebodamian@gmail.com>
2021-07-06 20:19:11 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default function Text(){
+    return(
+        <div className='tx_container'>
+            <h1>Tienda Loca</h1>
+            <p>Articulo 1</p>
+            <p>Articulo 2</p>
+            <p>Articulo 3</p>
+            <p>Articulo 4</p>
+            <p>Articulo 5</p>
+            <p>Articulo 6</p>
+        </div>
+    )
+}
---
+++ src/components/Text.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export default function Text(){
+    return(
+        <div className='tx_container'>
+            <h1>titulo loco de Text.js</h1>
+        </div>
+    )
+}
